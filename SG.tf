5e137ef588c26149165eb30a5a2ab5572987ea17
sumanthtony <sumanthbattagiri99@gmail.com>
2026-04-19 22:55:21 +0530

    Add AWS security group with all traffic rules
    Created a security group with ingress and egress rules for SSH, HTTP, and PostgreSQL traffic.

---
+++ SG.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "mysg" {
+name = var.sg_name
+description = "it has all traffic"
+
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 5432
+to_port = 5432
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+
