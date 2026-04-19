2883065e95afcb0ef9fbd3f0b3f61f51dc85e726
root <root@ip-172-31-44-140.ap-south-1.compute.internal>
2026-04-19 15:56:20 +0000

    modified variables.tf

--- variables.tf
+++ variables.tf
@@ -14,20 +14,17 @@ variable "itype" {
 }
 
 variable "vpc_cidr" {
-    type = number
+    type = string
     default = "11.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-    type = number
+    type = string
     default = "11.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
-    type = number
+    type = string
     default = "11.0.2.0/24"
 }
 
-variable "db_password" {
-  default = "password123"
-}
