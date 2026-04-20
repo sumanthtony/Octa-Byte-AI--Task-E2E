b03e3454940112630ff32edf3a2402b0638fe8dd
sumanthtony <sumanthbattagiri99@gmail.com>
2026-04-20 11:36:05 +0530

    Update RDS.tf

--- RDS.tf
+++ RDS.tf
@@ -22,3 +22,43 @@ resource "aws_security_group" "rds_sg" {
     Name = "iac-rds-sg"
   }
 }
+
+# DB SUBNET GROUP: RDS must be placed in subnets (prefer private)
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+tags = {
+    Name = "iac-rds-subnet-group"
+  }
+subnet_ids = [aws_subnet.mysn2.id]
+}
+#Because RDS must be associated with a subnet group to decide where it runs
+
+#RDS POSTGRESQL INSTANCE
+
+resource "aws_db_instance" "postgres" {
+  identifier = "iac-postgres-db"
+
+  engine         = "postgres"
+  engine_version = "14"
+  instance_class = "db.t3.micro"
+
+  allocated_storage = 20
+
+  db_name  = "mydb"
+  username = var.db_username
+  password = var.db_password
+
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  publicly_accessible = false   # IMPORTANT (private DB)
+  skip_final_snapshot = true
+
+  multi_az = false
+
+  tags = {
+    Name = "iac-postgres"
+  }
+}
+
+
