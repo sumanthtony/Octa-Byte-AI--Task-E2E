23395f8aeeadb26b2e2990724378511d77d65650
sumanthtony <sumanthbattagiri99@gmail.com>
2026-04-20 12:00:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+#RDS
 output "rds_endpoint" {
   value = aws_db_instance.postgres.endpoint
 }
@@ -5,3 +6,21 @@ output "rds_endpoint" {
 output "rds_port" {
   value = aws_db_instance.postgres.port
 }
+
+# EC2 Instance
+output "app_instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.one.id
+}
+
+output "app_public_ip" {
+  description = "EC2 Public IP"
+  value       = aws_instance.one.public_ip
+}
+
+# ALB
+output "load_balancer_dns" {
+  description = "ALB DNS Name"
+  value       = aws_lb.myalb.dns_name
+}
+
